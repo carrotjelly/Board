af67d84009eceef3046e81831650177d62786068
carrotjelly <dlwptptkddmf@naver.com>
2024-04-08 02:44:43 +0900

    Swagger API설정, 댓글 도메인/조회/등록

---
+++ src/main/java/com/example/board/service/ReplyService.java
@@ -0,0 +1,14 @@
+package com.example.board.service;
+
+import com.example.board.dto.PageRequestDTO;
+import com.example.board.dto.PageResponseDTO;
+import com.example.board.dto.ReplyDTO;
+
+public interface ReplyService {
+
+    Long register(ReplyDTO replyDTO);
+
+    ReplyDTO read(Long rno);
+
+    PageResponseDTO<ReplyDTO> getListOfBoard(Long bno, PageRequestDTO pageRequestDTO);
+}
